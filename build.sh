a1e68c5e1f46a1a71fdfbcc10b1782d4b6b53d91
prabhajayaraj <prabhajayaraj2000@gmail.com>
2026-03-10 00:05:04 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 echo "Building static HTML site..."
+git clone https://github.com/prabhajayaraj/garments-website-2
+git pull origin main
 mkdir -p build
-cp index.html build/
+cp ./garments-website-2/* build/
 echo "Build complete. Output in /build"
